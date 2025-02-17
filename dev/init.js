bf2893350565227b92315701153e7048c5cd11b6
Shamil Siddique <shamilsdq@live.com>
2025-02-17 16:50:42 +0530

    Updated dev ziva chat initialization to match requirements

--- dev/init.js
+++ dev/init.js
@@ -1,32 +1,55 @@
 import TarkaChat from "../src/main.js";
 import "./style.css";
 
-async function sendMessage(message) {
-  // API call
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return Promise.resolve([
-    {
-      type: "text",
-      message: `Received user message **${message}**`,
-    },
-    {
-      type: "tiles",
-      tiles_data: [
-        { title: "Report Name", url: "https://google.com" },
-        { title: "Report Name 2", url: "https://google.com" }
-      ]
-    }
-  ]);
+const createSession = async () => {
+  // const response = await fetch("loclhost:3000/api/start-session");
+  // const { sessionId } = await response.json();
+  // return sessionId;
+
+  return "mock-session-id";
+};
+
+const creatSubmitHandlerMethod = (sessionId) => {
+  const submitHandler = async (message) => {
+    console.log(sessionId, message);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return Promise.resolve([
+      {
+        type: "text",
+        message: `Received user message **${message}**`,
+      },
+      {
+        type: "tiles",
+        tiles_data: [
+          { title: "Report Name", url: "https://google.com" },
+          { title: "Report Name 2", url: "https://google.com" }
+        ]
+      }
+    ]);
+  }
+  return submitHandler;
 }
 
-const chat = TarkaChat.init({
-  title: "ZIVA",
-  botName: "ZIVA",
-  userName: "Hruser",
-  selectorId: "zivachatbot",
-  submitHandler: sendMessage,
-  expand: false,
-});
+const initializeChatbot = (submitHandler) => {
+  const chat = TarkaChat.init({
+    title: "ZIVA",
+    botName: "ZIVA",
+    userName: "Hruser",
+    selectorId: "zivachatbot",
+    submitHandler: submitHandler,
+    expand: false,
+  });
+  return chat;
+}
 
-const toggleButton = document.getElementById("toggle-ziva");
-toggleButton.onclick = chat.toggle;
+
+const init = async () => {
+  const sessionId = await createSession();
+  const submitHandler = await creatSubmitHandlerMethod(sessionId);
+  const chat = initializeChatbot(submitHandler);
+
+  const toggleButton = document.getElementById("toggle-ziva");
+  toggleButton.onclick = chat.toggle;
+}
+
+init();
